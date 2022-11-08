86a0e0ee97da9fc2a9915308fb153befe081f9a3
Diego Brito <diego.nobri@gmail.com>
2022-11-08 18:06:01 -0300

    Adicionar arquivos de projeto.

---
+++ IndxAPI/Controllers/PublicacaoController.cs
@@ -0,0 +1,47 @@
+﻿using IndxAPI.Data;
+using IndxAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IndxAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PublicacaoController : ControllerBase
+    {
+        private IndxContext _context;
+
+        public PublicacaoController(IndxContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult RecuperarPublicacoes()
+        {
+            return Ok(_context.Publicacoes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaPublicacaoPorId(int id)
+        {
+            Publicacao publicacao = _context.Publicacoes.FirstOrDefault(p => p.Id == id);
+            if(publicacao != null)
+                return Ok(publicacao);
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult AdicionaPublicacao([FromBody] Publicacao publicacao)
+        {
+            _context.Publicacoes.Add(publicacao);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(RecuperaPublicacaoPorId), new { Id = publicacao.Id }, publicacao);
+        }
+    }
+}
---
+++ IndxAPI/Data/IndxContext.cs
@@ -0,0 +1,19 @@
+﻿using IndxAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IndxAPI.Data
+{
+    public class IndxContext : DbContext
+    {
+        public IndxContext(DbContextOptions<IndxContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Publicacao> Publicacoes { get; set; }
+    }
+}
